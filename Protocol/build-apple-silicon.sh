36a58ec36141badc5d21957fe70b5e5705ae34dd
LEE GUN HO <jason@grampus.co>
2023-04-13 15:39:59 +0900

    프로토콜 정의 및 빌드 스크립트 수정

--- Protocol/build-apple-silicon.sh
+++ Protocol/build-apple-silicon.sh
@@ -1,1 +1,5 @@
-./protoc-22.2-osx-aarch_64/bin/protoc addressbook.proto --csharp_out . 
+# ./protoc-22.2-osx-aarch_64/bin/protoc addressbook.proto --csharp_out .
+FILENAME="Protocol"
+./protoc-3.12.3-osx-x86_64/bin/protoc $FILENAME.proto --csharp_out .
+
+/bin/cp -f $FILENAME.cs ../Server/$FILENAME.cs
