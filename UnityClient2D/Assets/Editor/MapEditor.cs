f7e7ce6862bcdd49e7124f28a0449c4a2b8b8d22
LEE GUN HO <jason@grampus.co>
2023-04-17 13:58:16 +0900

    Prefabs/Map 안에 있는 전체 맵에 대해 충돌영역 export 처리

--- UnityClient2D/Assets/Editor/MapEditor.cs
+++ UnityClient2D/Assets/Editor/MapEditor.cs
@@ -16,47 +16,55 @@ public class MapEditor
     [MenuItem("Tools/GenerateMap %#g")]
     private static void GenerateMap()
     {
-        GameObject go = GameObject.Find("Grid");
-        if (null == go)
-        {
-            Debug.LogError("not found Grid");
-            return;
-        }
+        // 특정 폴더 읽기
+        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Map");
 
-        Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
-        if (null == tm)
+        foreach (GameObject go in gameObjects)
         {
-            Debug.LogError("not found Tilemap_Collision");
-            return;
-        }
-        // List<Vector3Int> blocked = new ListStack<Vector3Int>();
-        // foreach (Vector3Int pos in tm.cellBounds.allPositionsWithin)
-        // {
-        //     TileBase tile = tm.GetTile(pos);
-        //     if (null == tile)
-        //         continue;
-        //     blocked.Add(pos);
-        // }
-        
-        using (var writer = File.CreateText("Assets/Resources/Map/output.txt"))
-        {
-            // map size 
-            writer.WriteLine(tm.cellBounds.xMin);
-            writer.WriteLine(tm.cellBounds.xMax);
-            writer.WriteLine(tm.cellBounds.yMin);
-            writer.WriteLine(tm.cellBounds.yMax);
-                
-            // 왼쪽 상단을 원점으로 (y+가 위,x-가 왼)
-            for(int y=tm.cellBounds.yMax;y>=tm.cellBounds.yMin;y--)
+            // GameObject go = GameObject.Find("Map");
+            if (null == go)
             {
-                for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
-                {
-                    TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
-                    writer.Write(null == tile ? "0" : "1");
-                }
-
-                writer.WriteLine();
+                Debug.LogError($"not found Map ${go.name}");
+                return;
             }
+
+            Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+            if (null == tm)
+            {
+                Debug.LogError("not found Tilemap_Collision");
+                return;
+            }
+            // List<Vector3Int> blocked = new ListStack<Vector3Int>();
+            // foreach (Vector3Int pos in tm.cellBounds.allPositionsWithin)
+            // {
+            //     TileBase tile = tm.GetTile(pos);
+            //     if (null == tile)
+            //         continue;
+            //     blocked.Add(pos);
+            // }
+
+            string fileName = $"Assets/Resources/Map/{go.name}.txt";
+            using (var writer = File.CreateText(fileName))
+            {
+                // map size 
+                writer.WriteLine(tm.cellBounds.xMin);
+                writer.WriteLine(tm.cellBounds.xMax);
+                writer.WriteLine(tm.cellBounds.yMin);
+                writer.WriteLine(tm.cellBounds.yMax);
+                    
+                // 왼쪽 상단을 원점으로 (y+가 위,x-가 왼)
+                for(int y=tm.cellBounds.yMax;y>=tm.cellBounds.yMin;y--)
+                {
+                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+                        writer.Write(null == tile ? "0" : "1");
+                    }
+
+                    writer.WriteLine();
+                }
+            }
+            Debug.Log($"{fileName} saved.");
         }
         
         Debug.Log("Complete");
