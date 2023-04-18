406afc2758b37b496cbf403204ba4e7df84e39c7
LEE GUN HO <jason@grampus.co>
2023-04-18 20:16:49 +0900

    Search AI (A*)

--- UnityClient2D/Assets/Scripts/Managers/Contents/ObjectManager.cs
+++ UnityClient2D/Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -1,41 +1,51 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager
 {
-    // TODO : pending
-    // private Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
-    private List<GameObject> _objects = new List<GameObject>();
+	//Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
+	List<GameObject> _objects = new List<GameObject>();
 
+	public void Add(GameObject go)
+	{
+		_objects.Add(go);
+	}
 
-    public void Add(GameObject go)
-    {
-        _objects.Add(go);
-    }
-    
-    public void Remove(GameObject go)
-    {
-        _objects.Remove(go);
-    }
+	public void Remove(GameObject go)
+	{
+		_objects.Remove(go);
+	}
 
-    public GameObject Find(Vector3Int cellPos)
-    {
-        foreach (GameObject obj in _objects)
-        {
-            CreatureController cc = obj.GetComponent<CreatureController>();
-            if (null == cc)
-                continue;
-            if (cc.CellPos == cellPos)
-                return obj;
-        }
+	public GameObject Find(Vector3Int cellPos)
+	{
+		foreach (GameObject obj in _objects)
+		{
+			CreatureController cc = obj.GetComponent<CreatureController>();
+			if (cc == null)
+				continue;
 
-        return null;
-    }
-    
-    public void Clear()
-    {
-        _objects.Clear();
-    }
-    
+			if (cc.CellPos == cellPos)
+				return obj;
+		}
+
+		return null;
+	}
+
+	public GameObject Find(Func<GameObject, bool> condition)
+	{
+		foreach (GameObject obj in _objects)
+		{
+			if (condition.Invoke(obj))
+				return obj;
+		}
+
+		return null;
+	}
+
+	public void Clear()
+	{
+		_objects.Clear();
+	}
 }
