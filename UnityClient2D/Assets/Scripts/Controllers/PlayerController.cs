9db8ec8dc150c8994c9553b93b9ed9057693ff8c
LEE GUN HO <jason@grampus.co>
2023-04-18 18:51:49 +0900

    화살 스킬

--- UnityClient2D/Assets/Scripts/Controllers/PlayerController.cs
+++ UnityClient2D/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,6 +7,82 @@ using static Define;
 public class PlayerController : CreatureController
 {
 	private Coroutine _coSkill;
+	private bool _isRangeSkill = false;
+	
+	
+	protected override void UpdateAnimation()
+	{
+		if (_state == CreatureState.Idle)
+		{
+			switch (_lastDir)
+			{
+				case MoveDir.Up:
+					_animator.Play("IDLE_BACK");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Down:
+					_animator.Play("IDLE_FRONT");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Left:
+					_animator.Play("IDLE_RIGHT");
+					_sprite.flipX = true;
+					break;
+				case MoveDir.Right:
+					_animator.Play("IDLE_RIGHT");
+					_sprite.flipX = false;
+					break;
+			}
+		}
+		else if (_state == CreatureState.Moving)
+		{
+			switch (_dir)
+			{
+				case MoveDir.Up:
+					_animator.Play("WALK_BACK");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Down:
+					_animator.Play("WALK_FRONT");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Left:
+					_animator.Play("WALK_RIGHT");
+					_sprite.flipX = true;
+					break;
+				case MoveDir.Right:
+					_animator.Play("WALK_RIGHT");
+					_sprite.flipX = false;
+					break;
+			}
+		}
+		else if (_state == CreatureState.Skill)
+		{
+			switch (_lastDir)
+			{
+				case MoveDir.Up:
+					_animator.Play(_isRangeSkill ? "ATTACK_WEAPON_BACK" : "ATTACK_BACK");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Down:
+					_animator.Play(_isRangeSkill ? "ATTACK_WEAPON_FRONT" : "ATTACK_FRONT");
+					_sprite.flipX = false;
+					break;
+				case MoveDir.Left:
+					_animator.Play(_isRangeSkill ? "ATTACK_WEAPON_RIGHT" : "ATTACK_RIGHT");
+					_sprite.flipX = true;
+					break;
+				case MoveDir.Right:
+					_animator.Play(_isRangeSkill ? "ATTACK_WEAPON_RIGHT" : "ATTACK_RIGHT");
+					_sprite.flipX = false;
+					break;
+			}
+		}
+		else
+		{
+
+		}
+	}
 	
 	protected override void Init()
 	{
@@ -63,9 +139,9 @@ public class PlayerController : CreatureController
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			print("space!!");
 			State = CreatureState.Skill;
-			_coSkill = StartCoroutine("CoStartPunch");
+			// _coSkill = StartCoroutine("CoStartPunch");
+			_coSkill = StartCoroutine("CoStartShootArrow");
 		}
 	}
 	
@@ -78,9 +154,28 @@ public class PlayerController : CreatureController
 			print(target.name);
 		}
 		
+		_isRangeSkill = false;
+		
 		// 대기시간
 		yield return new WaitForSeconds(0.5f);
 		State = CreatureState.Idle;
 		_coSkill = null;
 	}
+	
+	IEnumerator CoStartShootArrow()
+	{
+		GameObject go = Managers.Resource.Instantiate("Creature/Arrow");
+		ArrowController ac = go.GetComponent<ArrowController>();
+		ac.Dir = _lastDir;
+		ac.CellPos = CellPos;
+		
+		_isRangeSkill = true;
+		
+		// 대기시간
+		yield return new WaitForSeconds(0.3f);
+		State = CreatureState.Idle;
+		_coSkill = null;
+	}
+	
+	
 }
