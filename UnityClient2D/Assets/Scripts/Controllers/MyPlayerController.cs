c54d2f3bfd544c50208586047946df349dfd645b
LEE GUN HO <jason@grampus.co>
2023-04-25 14:18:06 +0900

    수정 : 방향키 처리

--- UnityClient2D/Assets/Scripts/Controllers/MyPlayerController.cs
+++ UnityClient2D/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -66,19 +66,19 @@ public class MyPlayerController : PlayerController
 	{
 		_moveKeyPressed = true;
 
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
 		{
 			Dir = MoveDir.Up;
 		}
-		else if (Input.GetKey(KeyCode.S))
+		else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
 		{
 			Dir = MoveDir.Down;
 		}
-		else if (Input.GetKey(KeyCode.A))
+		else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 		{
 			Dir = MoveDir.Left;
 		}
-		else if (Input.GetKey(KeyCode.D))
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 		{
 			Dir = MoveDir.Right;
 		}
