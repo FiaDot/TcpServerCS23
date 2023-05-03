412a45f92eddffae0e82bdbf329a23e3c66bbd92
LEE GUN HO <jason@grampus.co>
2023-05-03 15:08:29 +0900

    로컬 이동

--- UnityClient2D/Assets/Scripts/Controllers/NetCharacter.cs
+++ UnityClient2D/Assets/Scripts/Controllers/NetCharacter.cs
@@ -7,8 +7,9 @@ public class NetCharacter : MonoBehaviour
 {
 	public int Id { get; set; }
 
+	[SerializeField]
 	public float _speed = 5.0f;
-
+	
 	public bool IsMine { get; set; }
 
 	private NetMove _netMoveInfo = new NetMove();
@@ -19,10 +20,32 @@ public class NetCharacter : MonoBehaviour
 		set { _netMoveInfo = value; }
 	}
 
+	// public Vector3 Dir
+	// {
+	// 	get { return ToVector3(_netMoveInfo.Dir); }
+	// 	set
+	// 	{
+	// 		
+	// 	}
+	// }
+
+	private Vector3 inputDir;
+	
+	
 	Vector3 ToVector3(vector3Net netValue)
 	{
 		return new Vector3(netValue.X, netValue.Y, netValue.Z);
 	}
+
+	vector3Net ToVector3Net(Vector3 v)
+	{
+		vector3Net net = new vector3Net();
+		net.X = v.x;
+		net.Y = v.y;
+		net.Z = v.z;
+		return net;
+	}
+	
 	
 	// 서버 접속 후 초기 위치 지정
 	public void InitPos()
@@ -38,8 +61,44 @@ public class NetCharacter : MonoBehaviour
  
     void Update()
     {
-        
+	    if (IsMine)
+	    {
+		    InputMovement();
+	    }
+	    
     }
 
+    void InputMovement()
+    {
+	    Vector3 destPos = Vector3.zero;
+	    Vector3 dir = Vector3.zero;
+	    
+	    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+		{
+			dir = Vector3.up;
+		}
+		else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+		{
+			dir = Vector3.down;
+		}
+		else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			dir = Vector3.left;
+		}
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+	    {
+		    dir = Vector3.right;
+		}
+	    
+	    if ( dir != Vector3.zero )
+	    {
+			destPos += dir * (_speed * Time.deltaTime);
+			// Debug.Log(destPos);
+
+			transform.position += destPos;
+	    }
+    }
+
+    
     
 }
