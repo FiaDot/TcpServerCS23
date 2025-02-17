7bd64123a967626401de6c07e02c668a73f2051a
rootlee <fiadot@gmail.com>
2025-02-17 14:55:09 +0900

    IPv4 우선 접속

--- UnityClient2D/Assets/Scripts/Managers/Contents/NetworkManager.cs
+++ UnityClient2D/Assets/Scripts/Managers/Contents/NetworkManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 using Google.Protobuf;
+using System.Net.Sockets;
 
 public class NetworkManager
 {
@@ -21,16 +22,38 @@ public class NetworkManager
 
 	public void Init()
 	{
-		// DNS (Domain Name System)
+		// 호스트 이름 가져오기
 		string host = Dns.GetHostName();
-		IPHostEntry ipHost = Dns.GetHostEntry(host);
-		IPAddress ipAddr = ipHost.AddressList[0];
-		IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-		Debug.Log(endPoint);
+		// 호스트 엔트리 가져오기
+		IPHostEntry ipHost = Dns.GetHostEntry(host);		
+	
+		IPAddress ipAddr = null;
+
+		// AddressList를 순회하며 IPv4 주소 찾기
+		foreach (IPAddress address in ipHost.AddressList)
+		{									
+			// IPv4 주소인지 확인			
+			if (address.AddressFamily == AddressFamily.InterNetwork) // IPv4 확인
+			{				
+				ipAddr = address;
+				break; // IPv4 주소를 찾았으면 루프 종료
+			}
+		}
+		
+		// IPv4 주소를 찾지 못했을 경우 처리
+		if (ipAddr == null)
+		{
+			Debug.LogWarning("IPv4 주소를 찾을 수 없습니다. 첫 번째 주소를 사용합니다.");
+			ipAddr = ipHost.AddressList[0]; // 첫 번째 주소 (IPv6일 수 있음)로 대체
+		}
+			
+		IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+		
+		Debug.Log($"Connect to: {ipAddr}");
 		Connector connector = new Connector();
 
-		// deprecated....for web player
+		// [Deprecated] for web player
 		//Security.PrefetchSocketPolicy(ipAddr.ToString(), 7777); 
 		
 		connector.Connect(endPoint,
